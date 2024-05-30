5d7826256db311e9cf5ac96f7c3d4ed447bdade9
Cameron Riddell <cameron@dutc.io>
2024-05-30 14:35:51 -0700

    remove latex build from reproduce.sh

--- reproduce.sh
+++ reproduce.sh
@@ -1,9 +1,9 @@
 # Run the noetbook and create figures
-ipython LiqConstr.py
+ipython --classic --matplotlib=agg LiqConstr.py
 
 # compile latex file
-pdflatex LiqConstr.tex
-bibtex LiqConstr
-pdflatex LiqConstr.tex
-pdflatex LiqConstr.tex
-rm LiqConstr.aux LiqConstr.bbl LiqConstr.blg LiqConstr.dep LiqConstr.log LiqConstr.out
+# pdflatex LiqConstr.tex
+# bibtex LiqConstr
+# pdflatex LiqConstr.tex
+# pdflatex LiqConstr.tex
+# rm LiqConstr.aux LiqConstr.bbl LiqConstr.blg LiqConstr.dep LiqConstr.log LiqConstr.out
